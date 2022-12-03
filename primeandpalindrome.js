8aab03d5277e24574d688e1b2c0e0bd956fbcf30
Venkysanju246 <sanjuvenky246@gmail.com>
2022-12-03 11:57:32 +0530

    Written code for prime and palindrome

---
+++ primeandpalindrome.js
@@ -0,0 +1,32 @@
+
+
+//code to check prime number
+
+let num = 10
+let count =0
+
+for(let i =2;i<=num-1;i++){
+    if(num%i==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(prime number)
+}else {
+    console.log(Not a prime)
+}
+
+//code to chcek palindrome
+
+let str = "venky"
+let bag = ""
+for(let i = str.length-1;i<=0;i++){
+bag+=str[i]
+}
+if(bag==str){
+    console.log(palindrome)
+}else {
+    console.log(not a palindrome)
+}
+    
+
